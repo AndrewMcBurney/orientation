c8d465ba44a1da1857c2083bdf1e9bbf6baca11a
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-07-07 15:35:47 -0700

    Updated migrations for rollback.

--- db/migrate/20170629201802_update_category_association_table.rb
+++ db/migrate/20170629201802_update_category_association_table.rb
@@ -1,5 +1,5 @@
 class UpdateCategoryAssociationTable < ActiveRecord::Migration[5.0]
-  def change
+  def up
     drop_table :category_guide_associations
 
     create_table :articles_categories, id: false do |t|
@@ -8,5 +8,16 @@ class UpdateCategoryAssociationTable < ActiveRecord::Migration[5.0]
     end
 
     add_index :articles_categories, [:article_id, :category_id]
+  end
+
+  def down
+    drop_table :articles_categories
+
+    create_table :category_guide_associations do |t|
+      t.integer :category_id
+      t.integer :article_id
+
+      t.timestamps
+    end
   end
 end
