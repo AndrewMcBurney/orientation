f9797af64dd6a98c76dc9ee19c1711dc430ba00e
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-07-26 10:50:01 -0700

    Added custom ordering in database for categories.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170629204545) do
+ActiveRecord::Schema.define(version: 20170726172814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,9 +96,10 @@ ActiveRecord::Schema.define(version: 20170629204545) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "label"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.string   "slug"
+    t.integer  "order",      default: 0, null: false
     t.index ["slug"], name: "index_categories_on_slug", unique: true, using: :btree
   end
 
