c75af8ead63e229bc7e4e7907b0946296c97e66f
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-08-04 09:52:16 -0700

    Put 'User.administrator?' stub in 'Admin User' context in categories_controller_spec.rb.

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -5,91 +5,95 @@ require "rails_helper"
 RSpec.describe CategoriesController, type: :controller do
   let(:attributes) { { label: "Sample Category" } }
 
-  before do
-    allow_any_instance_of(User).to(receive(:administrator?).and_return(true))
-  end
+  context "Non-admin User" do
+    describe "GET #index" do
+      it "returns a success response" do
+        Category.create! attributes
+        get :index, params: {}
+        expect(response).to be_success
+      end
+    end
 
-  describe "GET #index" do
-    it "returns a success response" do
-      Category.create! attributes
-      get :index, params: {}
-      expect(response).to be_success
+    describe "GET #show" do
+      it "returns a success response" do
+        category = Category.create! attributes
+        get :show, params: { id: category.to_param }
+        expect(response).to be_success
+      end
     end
   end
 
-  describe "GET #show" do
-    it "returns a success response" do
-      category = Category.create! attributes
-      get :show, params: { id: category.to_param }
-      expect(response).to be_success
+  context "Admin User" do
+    before do
+      allow_any_instance_of(User).to(receive(:administrator?).and_return(true))
     end
-  end
 
-  describe "GET #new" do
-    it "returns a success response" do
-      get :new, params: {}
-      expect(response).to be_success
+    describe "GET #new" do
+      it "returns a success response" do
+        get :new, params: {}
+        expect(response).to be_success
+      end
     end
-  end
 
-  describe "GET #edit" do
-    it "returns a success response" do
-      category = Category.create! attributes
-      get :edit, params: { id: category.to_param }
-      expect(response).to be_success
+    describe "GET #edit" do
+      it "returns a success response" do
+        category = Category.create! attributes
+        get :edit, params: { id: category.to_param }
+        expect(response).to be_success
+      end
     end
-  end
 
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new Category" do
-        expect do
+    describe "POST #create" do
+      context "with valid params" do
+        it "creates a new Category" do
+          expect do
+            post :create, params: { category: attributes }
+          end.to change(Category, :count).by(1)
+        end
+
+        it "redirects to the created category" do
           post :create, params: { category: attributes }
-        end.to change(Category, :count).by(1)
-      end
-
-      it "redirects to the created category" do
-        post :create, params: { category: attributes }
-        expect(response).to redirect_to(Category.last)
+          expect(response).to redirect_to(Category.last)
+        end
       end
     end
-  end
 
-  describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) { { label: "New Category Name" } }
+    describe "PUT #update" do
+      context "with valid params" do
+        let(:new_attributes) { { label: "New Category Name" } }
 
-      it "updates the requested category" do
-        category = Category.create! attributes
-        put :update, params: { id: category.to_param, category: new_attributes }
+        it "updates the requested category" do
+          category = Category.create! attributes
+          put :update, params: { id: category.to_param, category: new_attributes }
 
-        category.reload
+          category.reload
 
-        expect(category.label).to eq("New Category Name")
-        expect(category.slug).to eq("new-category-name")
-      end
+          expect(category.label).to eq("New Category Name")
+          expect(category.slug).to eq("new-category-name")
+        end
 
-      it "redirects to the category" do
-        category = Category.create! attributes
-        put :update, params: { id: category.to_param, category: attributes }
-        expect(response).to redirect_to(category)
+        it "redirects to the category" do
+          category = Category.create! attributes
+          put :update, params: { id: category.to_param, category: attributes }
+          expect(response).to redirect_to(category)
+        end
       end
     end
-  end
 
-  describe "DELETE #destroy" do
-    it "destroys the requested category" do
-      category = Category.create! attributes
+    describe "DELETE #destroy" do
+      it "destroys the requested category" do
+        category = Category.create! attributes
 
-      expect do
+        expect do
+          delete :destroy, params: { id: category.to_param }
+        end.to change(Category, :count).by(-1)
+      end
+
+      it "redirects to the categories list" do
+        category = Category.create! attributes
         delete :destroy, params: { id: category.to_param }
-      end.to change(Category, :count).by(-1)
-    end
-
-    it "redirects to the categories list" do
-      category = Category.create! attributes
-      delete :destroy, params: { id: category.to_param }
-      expect(response).to redirect_to(categories_url)
+        expect(response).to redirect_to(categories_url)
+      end
     end
   end
 end
